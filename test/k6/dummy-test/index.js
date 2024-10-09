4d9a8df24f14eae975ae63d6e9713ef201a5ebd9
Shikhar Yadav <yshikharfzd10@gmail.com>
2024-10-09 11:18:16 +0530

    test: new scenanrio tested

--- test/k6/dummy-test/index.js
+++ test/k6/dummy-test/index.js
@@ -2,23 +2,29 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    vus: 1000, // number of virtual users
-    duration: '10s', // test duration
+    stages: [
+        { duration: '5m', target: 200 },  // Ramp-up to 200 VUs in 5 minutes
+        { duration: '10m', target: 1000 }, // Ramp-up to 1000 VUs in 10 minutes
+        { duration: '10m', target: 1000 }, // Maintain 1000 VUs for 10 minutes (max load)
+        { duration: '5m', target: 2000 }, // Spike to 2000 VUs in 5 minutes
+        { duration: '2m', target: 2000 }, // Hold at 2000 VUs for 2 minutes (traffic spike)
+        { duration: '5m', target: 1000 }, // Scale down to 1000 VUs in 5 minutes
+        { duration: '5m', target: 0 },    // Gradually ramp-down to 0 VUs
+    ],
 };
 
-const API_URL = 'http://127.0.0.1:80';
+const API_URL = 'http://13.233.233.119';
 
 export default function () {
     // Home
     let readRes = http.get(`${API_URL}/`);
     check(readRes, { 'read status was 200': (r) => r.status === 200 });
 
-    // // Create
+    // Create
     let createPayload = JSON.stringify({ id: '1', value: 'test value' });
     let createHeaders = { 'Content-Type': 'application/json' };
     let createRes = http.post(`${API_URL}/create`, createPayload, { headers: createHeaders });
     check(createRes, { 'create status was 201': (r) => r.status === 201 });
-
 
     // Update
     let updatePayload = JSON.stringify({ id: '1', value: 'updated value' });
